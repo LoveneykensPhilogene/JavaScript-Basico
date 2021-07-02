ab8ab9c25da5030a14cd7a6f8e4273beaa850a58
Loveneykens Philogene <loveneykens@gmail.com>
2021-07-02 10:48:53 -0300

    Array nome

--- Script_basico/script.js
+++ Script_basico/script.js
@@ -2,3 +2,5 @@ var nome = "Jean";
 var idade = 34;
 var sexo = "homem";
 document.write("Nome :" + nome + "<br/>" + "Idade : " + idade + "<br/>" + "Sexo : " + sexo);
+var nome = [nome, idade, sexo];
+document.write("<br/>" + nome);
