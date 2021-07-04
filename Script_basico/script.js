30e5b161375e2b5151463cf60a61c4be7c6e04de
Loveneykens Philogene <loveneykens@gmail.com>
2021-07-04 18:58:39 -0300

    JavaScript Basic

--- Script_basico/script.js
+++ Script_basico/script.js
@@ -1,6 +1,17 @@
 var nome = "Jean";
 var idade = 34;
 var sexo = "homem";
+
 document.write("Nome :" + nome + "<br/>" + "Idade : " + idade + "<br/>" + "Sexo : " + sexo);
+//array
 var nome = [nome, idade, sexo];
 document.write("<br/>" + nome);
+
+//laços
+document.write("<br />");
+document.write("<br />" + "Contagem : ");
+for (a = 0; a <= 10; a++) {
+  var valor_a = [a];
+  //  if (a == 0)
+  document.write(valor_a + "-");
+}
